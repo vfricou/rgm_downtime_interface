f5a4b24ae1aebd1fb90cb6d44cdefdf6776a20ef
Vincent FRICOU <vincent@fricouv.eu>
2020-06-12 18:49:41 +0200

    test: datetimepicker

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RgmDowntimeInterface
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Load custom javascript
+    config.assets.paths << "#{Rails.root}/app/assets/javascript"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
