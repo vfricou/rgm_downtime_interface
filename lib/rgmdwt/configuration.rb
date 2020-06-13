e101e8fe24baab687095507eef07ed3f947d6c40
Sébastien DEU <sebastien@sdeu.fr>
2020-06-13 16:00:16 +0200

    feat: add file_name for list_apps

--- lib/rgmdwt/configuration.rb
+++ lib/rgmdwt/configuration.rb
@@ -4,7 +4,7 @@ module Rgmdwt
       config_array = []
       config_files = Dir.entries(Rails.root.join('app_configs')).reject { |f| File.directory? f }
       config_files.each do |app|
-        config_array << YAML.load_file(Rails.root.join('app_configs') + app)
+        config_array << YAML.load_file(Rails.root.join('app_configs') + app).merge({file_name: app})
       end
 
       config_array
