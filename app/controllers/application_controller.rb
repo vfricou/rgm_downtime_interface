ee3192bcba7d22647433ea42daf27ddde8d28829
Vincent FRICOU <vincent@fricouv.eu>
2020-06-13 17:22:29 +0200

    feat: view config page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-    def index
-    end
-    def view_config
-    end
+  def index
+  end
+  def view_conf
+    @app_config = Rgmdwt::Configuration.get_config(params[:id])
+  end
 end
--- lib/rgmdwt/configuration.rb
+++ lib/rgmdwt/configuration.rb
@@ -4,18 +4,19 @@ module Rgmdwt
       config_array = []
       config_files = Dir.entries(Rails.root.join(Rails.configuration.rgmdwt[:app_config])).reject { |f| File.directory? f }
       config_files.each do |app|
-        config_array << YAML.load_file(Rails.root.join(Rails.configuration.rgmdwt[:app_config]) + app).merge({file_name: app})
+        config_array << YAML.load_file(Rails.root.join(Rails.configuration.rgmdwt[:app_config]) + app).merge({file_name: app.split('.').first})
       end
 
       config_array
     end
 
     def self.get_config(app_file)
-      config_file = Rails.root.join(Rails.configuration.rgmdwt[:app_config]) + app_file
+      config_file = Rails.root.join(Rails.configuration.rgmdwt[:app_config]) + "#{app_file}.yml"
       puts config_file.inspect
       yamlContent = YAML.load_file(config_file)
       #puts yamlContent.inspect
       #return yamlContent
+      return yamlContent
     end
   end
 end
