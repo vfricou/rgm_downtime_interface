6133228d8a97de953e2f8d2a99943cab8aa12bbf
Vincent Fricou <vincent.fricou@nimes.fr>
2022-02-22 16:28:31 +0100

    updt: don’t generate one token by api call to accelerate

--- lib/rgmdwt/api.rb
+++ lib/rgmdwt/api.rb
@@ -3,8 +3,9 @@ module Rgmdwt
     def self.create_downtime(comment, start_time, end_time, hostname, service = nil)
       rgmapi_endpoint = service.nil? ? 'createHostDowntime' : 'createServiceDowntime'
       @payload = Utils.dwt_payload(comment, start_time, end_time, hostname, service)
+      token=Utils.api_token
       api_request = RestClient::Resource.new(
-        "https://#{Rails.configuration.rgmdwt[:api_server]}/rgmapi/#{rgmapi_endpoint}?token=#{Utils.api_token}",
+        "https://#{Rails.configuration.rgmdwt[:api_server]}/rgmapi/#{rgmapi_endpoint}?token=#{token}",
         verify_ssl: OpenSSL::SSL::VERIFY_NONE
       ).post(@payload.to_json, { content_type: 'application/json' })
     end
@@ -22,9 +23,10 @@ module Rgmdwt
       else
         @request.merge!({filters: ["host_name = #{hostname}", "service_description = #{service}"]})
       end
+      token = Utils.api_token
       dwt_entry = JSON.parse(
         RestClient::Resource.new(
-          "https://#{Rails.configuration.rgmdwt[:api_server]}/rgmapi/listNagiosObjects?token=#{Utils.api_token}",
+          "https://#{Rails.configuration.rgmdwt[:api_server]}/rgmapi/listNagiosObjects?token=#{token}",
           verify_ssl: OpenSSL::SSL::VERIFY_NONE
         ).post(@request.to_json, { content_type: 'application/json' }).body
       )
